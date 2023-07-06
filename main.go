5225b67494cf4bed20c910bdb559b2428e89db94
abinashmahapatra <abinashmahapatra41@gmail.com>
2023-07-06 23:27:41 +0530

    Command Arguments With Conditionals

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 
 func main() {
 	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./hello-world <argument>\n")
+		os.Exit(1)
+
+	}
 	fmt.Printf("Hello World\nos.Args: %v\nArgument: %v\n", args, args[1])
 
 }
